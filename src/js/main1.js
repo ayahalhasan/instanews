bd325da5373ff80c5180ec1224e737e86755be1c
Ayah Al-Hasan <ayahal-hasan@Ayahs-MacBook-Air.local>
2016-09-15 11:14:29 -0700

    make some changes in files

---
+++ src/js/main1.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  // Function that calls the NYT API
+  var $select = $('select'); // Dropdown Select manue 
+  $select.on('change', function(event) {
+  event.preventDefault();
+  var $section = $('.stories'); // HTML element where we will insart articles
+  $section.empty();
+  var input = $('select option:selected').val();
+  // console.log(input);
+
+  //API variable key
+  var urlForAPI = "https://api.nytimes.com/svc/topstories/v2/" + input +
+  ".json?api-key=e6f99b5e399d472fbbf80ccef5d5a29c"; // Url for API call
+
+  //Ajax Request
+  $.ajax({
+    method: 'GET',
+    url: urlForAPI,
+    dataType: 'json',
+  })
+  .done(function(data){
+    let nyt = data.results;
+    nyt = nyt.filter(function(item) {
+      return item.multimedia.length;
+    });
+    nyt = nyt.splice(0,12);
+
+    let articles = ''; // String of articles for HTML
+
+    $.each(nyt, (index, value) => {
+      let link = value.url;
+      let imgUrl = value.multimedia[4].url;
+      let abstract = value.abstract;
+
+
+      articles += `<article>
+                    <a href="${link}">Link ${index +1};
+                      <div class="inner" style="background: url(${imgUrl}); background-size: cover">
+                        <p>${abstract}</p>
+                      </div>
+                    </a>
+                   </article>`
+    });
+
+    $section.append(articles);
+  })
+  .always(function(){
+
+  });
+
+
+  });
+
+});
